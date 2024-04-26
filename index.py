d52fddfa2226c02e2670133a944f4f61bb68cb48
Gabriel Cabral <gabriel.alonso.dev@icloud.com>
2024-04-26 19:07:24 -0300

    initial commit

---
+++ index.py
@@ -0,0 +1,78 @@
+# For running inference on the TF-Hub module.
+import tensorflow as tf
+import tensorflow_hub as hub
+
+
+# For drawing onto the image.
+from utils import display_image, download_and_resize_image, draw_boxes, draw_bounding_box_on_image
+
+# For measuring the inference time.
+import time
+
+# Print Tensorflow version
+print(tf.__version__)
+
+# Check available GPU devices.
+print("The following GPU devices are available: %s" %
+      tf.test.gpu_device_name())
+
+
+image_url = "https://upload.wikimedia.org/wikipedia/commons/6/60/Naxos_Taverna.jpg"
+
+# @param ["https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1", "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1"]
+module_handle = "https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1"
+
+detector = hub.load(module_handle).signatures['default']
+
+
+def load_img(path):
+    img = tf.io.read_file(path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    return img
+
+
+def run_detector(detector, path):
+    img = load_img(path)
+
+    converted_img = tf.image.convert_image_dtype(img, tf.float32)[
+        tf.newaxis, ...]
+    start_time = time.time()
+    result = detector(converted_img)
+    end_time = time.time()
+
+    result = {key: value.numpy() for key, value in result.items()}
+    print(result)
+    print("Found %d objects." % len(result["detection_scores"]))
+    print("Inference time: ", end_time-start_time)
+
+    detection_classes = result["detection_class_entities"]
+    detection_scores = result["detection_scores"]
+
+    for class_entity, score in zip(detection_classes, detection_scores):
+        print("Class:", class_entity.decode("utf-8"), "| Score:", score)
+
+    image_with_boxes = draw_boxes(
+        img.numpy(), result["detection_boxes"],
+        result["detection_class_entities"], result["detection_scores"])
+
+    display_image(image_with_boxes)
+
+    # run_detector(detector, downloaded_image_path)
+
+
+image_urls = [
+    "https://upload.wikimedia.org/wikipedia/commons/1/1b/The_Coleoptera_of_the_British_islands_%28Plate_125%29_%288592917784%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Biblioteca_Maim%C3%B3nides%2C_Campus_Universitario_de_Rabanales_007.jpg/1024px-Biblioteca_Maim%C3%B3nides%2C_Campus_Universitario_de_Rabanales_007.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/0/09/The_smaller_British_birds_%288053836633%29.jpg",
+]
+
+
+def detect_img(image_url):
+    start_time = time.time()
+    image_path = download_and_resize_image(image_url, 640, 480)
+    run_detector(detector, image_path)
+    end_time = time.time()
+    print("Inference time:", end_time-start_time)
+
+
+detect_img(image_urls[0])
